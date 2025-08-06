259449e5c61083245549c0331628f44241f3ec9b
Gino <me@gino.dev>
2025-08-06 11:44:42 +0200

    adding inertia to have react frontend + tailwind

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -1,12 +1,12 @@
 import { useAuth } from "@/lib/hooks/useAuth";
 
 export default function Index() {
-    const foo = useAuth();
+    const user = useAuth();
 
     return (
         <div>
-            <div className="bg-blue-50">foo</div>
-            <div className="bg-pink-50">{JSON.stringify(foo)}</div>
+            <div className="bg-blue-50">logged in</div>
+            <div className="bg-pink-50">{JSON.stringify(user)}</div>
         </div>
     );
 }
