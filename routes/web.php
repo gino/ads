f809587475fac846b23f3cb7c73891e967cd43fd
Gino <me@gino.dev>
2025-08-06 11:18:12 +0200

    adding inertia to have react frontend + tailwind

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SyncController;
+use App\Http\Controllers\ViewController;
 use App\Http\Middleware\EnsureFacebookTokenIsValid;
 use App\SyncType;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // https://developers.facebook.com/docs/marketing-api/get-started/basic-ad-creation
@@ -16,14 +16,10 @@ Route::middleware('guest')->group(function () {
 Route::get('/connect/facebook/callback', [AuthController::class, 'callback']);
 
 Route::middleware(['auth', EnsureFacebookTokenIsValid::class])->group(function () {
-    Route::get('/foo', function () {
-        $user = Auth::user();
-
-        return $user;
-    });
+    Route::get('/', [ViewController::class, 'index'])->name('dashboard.index');
 
     // Will be a POST eventually
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
     Route::get('/force-sync/{type?}', [SyncController::class, 'sync'])->whereIn('type', array_map(fn ($e) => $e->value, SyncType::cases()));
 });
